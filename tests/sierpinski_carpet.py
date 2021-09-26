8d1dcce91e52b9d1341c2cc9c89d7e806016ee65
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2021-09-26 12:30:33 -0400

    sierpinski_carpet.py: Send images to a displaty server

--- tests/sierpinski_carpet.py
+++ tests/sierpinski_carpet.py
@@ -3,6 +3,7 @@ from fractal.generation import Plot
 import logging
 import os
 import cv2
+import imagezmq
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s [%(levelname)s] %(message)s')
 
@@ -14,19 +15,25 @@ def main():
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
+    sender = imagezmq.ImageSender(connect_to="tcp://127.0.0.1:5555", REQ_REP=True)
+
     sierpiski_generator = SierpinskiCarpet()
     logging.info("Generating points...")
     points = sierpiski_generator.GeneratePoints(number_of_points=number_of_points, number_of_points_to_ignore=10)
     image = Plot(points, image_sizeHW)
+    sender.send_image("sierpinski", image)
+
     for step in range(10):
         logging.info("Generating points...")
         new_points = sierpiski_generator.GeneratePoints(number_of_points=number_of_points,
                                                         number_of_points_to_ignore=0, first_point=points[-1])
         logging.info("Done!")
-        points = points + new_points
+        #points = points + new_points
         logging.info("Plotting in an image...")
-        image = Plot(points, image_sizeHW)
+        image = Plot(new_points, image=image)
         logging.info("Done!")
+
+        sender.send_image("sierpinski", image)
     image_filepath = os.path.join(output_directory, "sierpinski_{}.png".format(number_of_points))
     cv2.imwrite(image_filepath, image)
 

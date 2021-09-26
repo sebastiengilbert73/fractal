c8c40eed9e194ef3e5cd126ffeb7e9e4594a4e59
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2021-09-26 11:23:30 -0400

    sierpinski_carpet.py: Add points to the image in small batches

--- tests/sierpinski_carpet.py
+++ tests/sierpinski_carpet.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s [%(levelname)s] 
 def main():
     logging.info("sierpinski_carpet.py main()")
     output_directory = './outputs'
-    number_of_points = 1000000
+    number_of_points = 100000
     image_sizeHW= (1024, 1024)
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
@@ -17,10 +17,16 @@ def main():
     sierpiski_generator = SierpinskiCarpet()
     logging.info("Generating points...")
     points = sierpiski_generator.GeneratePoints(number_of_points=number_of_points, number_of_points_to_ignore=10)
-    logging.info("Done!")
-    logging.info("Plotting in an image...")
     image = Plot(points, image_sizeHW)
-    logging.info("Done!")
+    for step in range(10):
+        logging.info("Generating points...")
+        new_points = sierpiski_generator.GeneratePoints(number_of_points=number_of_points,
+                                                        number_of_points_to_ignore=0, first_point=points[-1])
+        logging.info("Done!")
+        points = points + new_points
+        logging.info("Plotting in an image...")
+        image = Plot(points, image_sizeHW)
+        logging.info("Done!")
     image_filepath = os.path.join(output_directory, "sierpinski_{}.png".format(number_of_points))
     cv2.imwrite(image_filepath, image)
 
